da157fee3b46990a74a6193bafd2ca46d99273a7
karo-fox <karolina.kozubik@gmail.com>
2023-03-04 12:51:04 +0100

    integrate with tui

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,13 +1,37 @@
-use crate::app::App;
-use crossterm::event::{KeyCode, KeyEvent};
+use crate::app::{App, InputMode};
+use crossterm::event::{KeyCode, KeyEvent, KeyEventKind};
 
 pub struct EventHandler;
 
 impl EventHandler {
     pub fn on_key(key: KeyEvent, app: &mut App) {
-        match key.code {
-            KeyCode::Esc | KeyCode::Char('q') => app.is_running = false,
-            _ => (),
+        if key.kind == KeyEventKind::Press {
+            match app.input_mode {
+                InputMode::Command => match key.code {
+                    KeyCode::Esc | KeyCode::Char('q') | KeyCode::Char('Q') => {
+                        app.is_running = false
+                    }
+                    KeyCode::Up => app.state.tasks.previous(),
+                    KeyCode::Down => app.state.tasks.next(),
+                    KeyCode::Left => app.state.tasks.unselect(),
+                    KeyCode::Char('a') | KeyCode::Char('A') => app.input_mode = InputMode::AddTask,
+                    _ => (),
+                },
+                InputMode::AddTask => match key.code {
+                    KeyCode::Enter => {
+                        app.state.add_task();
+                        app.input_mode = InputMode::Command;
+                    }
+                    KeyCode::Char(c) => {
+                        app.state.text_input.push(c);
+                    }
+                    KeyCode::Backspace => {
+                        app.state.text_input.pop();
+                    }
+                    KeyCode::Esc => app.input_mode = InputMode::Command,
+                    _ => (),
+                },
+            }
         }
     }
 }
