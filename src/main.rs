9360ce87ad5e9e3012cb9ca2fde5c287432416bd
KGuz <42916624+KGuz@users.noreply.github.com>
2023-03-17 16:55:37 +0100

    feat: add custom panic hook

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use anyhow::Result;
 use crossterm::event;
 use plan_ahead::{application::App, backend::terminal::Terminal, ui::stateful_ui::StatefulUi};
+use std::panic;
 
 fn main() -> Result<()> {
+    panic::set_hook(Box::new(panic_hook));
+
     let mut terminal = Terminal::new()?;
     let mut app = App::default();
     let mut ui = StatefulUi::default();
@@ -15,4 +18,12 @@ fn main() -> Result<()> {
         handler.on_event(event::read()?, &mut app, &mut ui);
     }
     Ok(())
+}
+
+fn panic_hook(info: &panic::PanicInfo<'_>) {
+    use crossterm::{execute, terminal};
+
+    terminal::disable_raw_mode().unwrap();
+    execute!(std::io::stdout(), terminal::LeaveAlternateScreen).unwrap();
+    println!("{info}");
 }
